bb9e0c096166951c01506c309feef750fad25626
bombusfragrans <97529136+bombusfragrans@users.noreply.github.com>
2022-09-27 13:36:58 +0300

    Add files via upload

--- exercises/robot-simulator.jl
+++ exercises/robot-simulator.jl
@@ -1,3 +1,7 @@
+# required imports
+
+import Base: ==
+
 # structure
 
 mutable struct Point{T <: Int}
@@ -32,34 +36,33 @@ end
 #--------------
 # constructors
 
-Point(t::Tuple{Int, Int}) = Point(t...)	# PASSED
-
 # Point(x::Int, y::Int) dispatched to default constructor
 
-Robot(t::Tuple{Int,Int},h::Heading)::Robot = Robot(Point(t),h)	# requires custom convert?
+Robot(t::Tuple{Int,Int},H::Type{<:Heading})::Robot = Robot(Point(t...),H)	
 
-# Robot(p::Point,h::HEADING) dispatched to default constructor
+# Robot(p::Point,H::HEADING) dispatched to default constructor
 
 #------------------
 # methods / traits
 
-position(r::Robot)::Point = r.position
+position(R::Robot)::Point = R.position
 
-heading(r::Robot)::Heading = r.heading
+heading(R::Robot)::Type{<:Heading} = R.heading 
 
 # just forthe record: could have been done by a macro, but would be less explicit and thus more challenging to read
 
-turn_right!(r::Robot) = r.heading = _turn_right(r.heading)
-
 _turn_right(::Type{NORTH}) = EAST
 
-_turn_right(::type{EAST}) = SOUTH
+_turn_right(::Type{EAST}) = SOUTH
 
 _turn_right(::Type{SOUTH}) = WEST
 
 _turn_right(::Type{WEST}) = NORTH
 
-turn-left!(r::Robot) = r.heading = _turn_left(r.heading)
+function turn_right!(R::Robot)
+    R.heading = _turn_right(heading(R))
+    return R
+end
 
 _turn_left(::Type{NORTH}) = WEST
 
@@ -68,6 +71,11 @@ _turn_left(::Type{WEST}) = SOUTH
 _turn_left(::Type{SOUTH}) = EAST
 
 _turn_left(::Type{EAST}) = NORTH
+
+function turn_left!(R::Robot)
+    R.heading = _turn_left(heading(R))
+    return R
+end
 
 #=
 # optional: alternative approach to `turn_right` & `turn_left`
@@ -79,29 +87,45 @@ const DIRECTIONS = (NORTH,EAST,SOUTH,WEST)
 # ???more julianic/julianesc using dispatch???
 =#
 
-advance!(r::Robot) = _advance!(r.heading,r)
+_advance!(::Type{NORTH}, R::Robot) = R.position.y += 1	
 
-_advance!(::Type{NORTH}, r::Robot) = r.position.x += 1	# TODO: recheck delegation pattern
+_advance!(::Type{EAST}, R::Robot) = R.position.x += 1
 
-_advance!(::Type{EAST}, r::Robot) = r.postion.y += 1
+_advance!(::Type{SOUTH}, R::Robot) = R.position.y -= 1
 
-_advance!(::Type{SOUTH}, r::Robot) = r.postion.y -= 1
+_advance!(::Type{WEST}, R::Robot) = R.position.x -= 1
 
-_advance!(::Type{WEST}, r::Robot) = r.position.x -= 1
+function advance!(R::Robot)
+    _advance!(heading(R),R)
+    return R
+end
 
-# just a reminder (for the record): setting `r.x` & `r.y` could be done with proper setter functions which mightbe a little overkill for this case 
+# just a reminder (for the record): setting `R.position.x` & `R.position.y` could be done with proper setter functions (using the delegate pattern)
 
-move!(r::Robot,s::String) = move!(r, popfirst!(s)); !isempty(s) && move!(r, s)
+_move!(::Type{Val{:A}},R::Robot) = advance!(R)
 
-move!(r::Robot,c::Char) = _move!(Symbol(c),r)
+_move!(::Type{Val{:R}},R::Robot) = turn_right!(R)
 
-_move!(::Val{:A},r::Robot) = advance!(r)
+_move!(::Type{Val{:L}},R::Robot) = turn_left!(R) 
 
-_move!(::Val{:R},r::Robot) = turn_right!(r)
+function move!(R::Robot,c::Char)
+    _move!(Val{Symbol(c)},R)
+    return R
+end  
 
-_move!(::Val{:L},r::Robot) = turn_left!(r)
+function move!(R::Robot,v::Vector{Char})
+    foreach(c -> move!(R,c), v)
+    return R
+end
+
+function move!(R::Robot,s::String)
+    move!(R,collect(s))
+    return R
+end
 
 #----------------------
 # 'overloaded' methods
 
-popfirst!(s::String) = popfirst!(collect(s))
+==(a::T,b::T) where {T<:Point} = a.x == b.x && a.y == b.y
+
+==(a::T,b::T) where {T<:Robot} = position(a) == position(b) && heading(a) == heading(b)
