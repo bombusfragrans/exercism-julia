1214b5c9546258aa8be0c983414a940a9f1fc20e
bombusfragrans <97529136+bombusfragrans@users.noreply.github.com>
2024-01-02 20:49:25 +0300

    minor refactoring

--- exercises/largest-series-product.jl
+++ exercises/largest-series-product.jl
@@ -1,14 +1,14 @@
 function largest_product(str, span)
     (span < 0 || 
-    length(str) < span ||   # also covers case: 'empty string and nonzero span'
+    length(str) < span ||   # also covers case: 'empty string and nonzero span
     any(!isnumeric, str)) &&
     throw(ArgumentError("Arguments invalid"))
-    [begin
-        str[i:min(i + span - 1, end)] |>
-        collect |> 
-        v -> parse.(Int, v) |>     # collect and broadcasting required to preserve leading zeros
+    collect(str) |> 
+    v -> parse.(Int, v) |>  # collect and broadcasting is required to preserve leading zeros 
+    s -> [begin
+        s[i:min(i + span - 1, end)] |>
         prod 
     end
-    for i in 1:length(str)-(span - 1)] |>
+    for i in 1:length(s)-(span - 1)] |>
     maximum # if all digits are zero or all chunks contain zero then the maximum of all products is also zero
 end
